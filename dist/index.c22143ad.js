3d0cda7e362f42d0029206141c05faee92eb0503
MaksymLytvyn <maxim.lytvyn@gmail.com>
2022-12-06 19:45:25 +0100

    43
    day2

---
+++ dist/index.c22143ad.js
@@ -0,0 +1,8 @@
+const mobileNav = document.querySelector("#hamburger .mobile");
+const button = document.querySelector("#hamburger");
+button.addEventListener("click", function() {
+    mobileNav.classList.toggle("active");
+    button.classList.toggle("active");
+});
+
+//# sourceMappingURL=index.c22143ad.js.map
